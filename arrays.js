857b949d58b883fc6a2c7e1e8886ebcc58800a14
yona l <04l.yona@gmail.com>
2018-01-18 06:06:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray (array, element) {
   return [element, ...array];
 }
+
 function destructiveAddElementToBeginningOfArray (array, element){
   array.unshift(element);
   return array;
