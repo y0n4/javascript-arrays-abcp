2d1e6018ed9e1be9f32ac513f36046fd4ac63c71
yona l <04l.yona@gmail.com>
2018-01-18 06:17:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,14 +1,35 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-//push and unshift MUTATE + returns number of elements in array
 
+
+//push and unshift MUTATE + returns number of elements in array
 function addElementToBeginningOfArray (array, element) {
   //spread operator(...) COPIES ['object', ...arrayName]
   return [element, ...array];
 }
-
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
+
+
+
+function addElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
+
+
+
+
+
+
+
+
+
+
+
 //remove element from mid of array >> return copy of og array
