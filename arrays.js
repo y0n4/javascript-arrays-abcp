770c2cd89d0a1e0545dc0d5ff0e590c844118fb3
yona l <04l.yona@gmail.com>
2018-01-18 06:21:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,17 @@ function destructivelyAddElementToEndOfArray(array, element){
 
 
 
-
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift();
+  return array;
+}
+
+
+
+
