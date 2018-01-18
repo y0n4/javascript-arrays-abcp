8b641c9cece0f4503c342033832e477c0bfe23dc
yona l <04l.yona@gmail.com>
2018-01-18 06:14:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,4 +7,8 @@ function addElementToBeginningOfArray (array, element) {
   return [element, ...array];
 }
 
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
 //remove element from mid of array >> return copy of og array
