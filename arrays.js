05c78f4779413164addb40e867f0100e9f3077ed
yona l <04l.yona@gmail.com>
2018-01-18 06:02:28 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+//push and unshift MUTATE + returns number of elements in array
+//spread operator (...) COPY ['object', ...arayName]
+
+function addElementToBeginningOfArray (array, element) {
+  return [element, ...array];
+}
+function destructiveAddElementToBeginningOfArray (array, element){
+  array.unshift(element);
+  return array;
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift();
+  return array;
+}
+
+//slice COPY and return the length element (single num)
+//return array thats kept (double num)
+
+function removeElementFromBeginningOfArray(array){
+  // or array.slice(1, lastIndex);
+  var newArray = array.slice(1);
+  return newArray;
+}
+
+//push MUTATES
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.push();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  var newArray = array.slice(0, array.length-1)
+  return newArray;
+}
+
+//remove element from mid of array >> return copy of og array
